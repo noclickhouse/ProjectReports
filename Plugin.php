1be2651e16aae2c5ae52156a687829a0ac58a04c
askdh-b <rustpy2703@gmail.com>
2024-05-16 11:49:02 +0600

    initialize plugin

---
+++ Plugin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Kanboard\Plugin\ProjectReports;
+
+use Kanboard\Core\Plugin\Base;
+use Kanboard\Core\Translator;
+
+class Plugin extends Base
+{
+    public function initialize()
+    {
+    }
+
+    public function onStartup()
+    {
+        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
+    }
+
+    public function getPluginName()
+    {
+        return 'ProjectReports';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('Generation a reports for the selected project');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Rustam Urazov';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/noclickhouse/ProjectReports';
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,31 @@
+ProjectReports
+==============================
+
+Generation a reports for the selected project
+
+Author
+------
+
+- Rustam Urazov
+- License MIT
+
+Requirements
+------------
+
+- Kanboard >= 1.0.35
+
+Installation
+------------
+
+You have the choice between 3 methods:
+
+1. Install the plugin from the Kanboard plugin manager in one click
+2. Download the zip file and decompress everything under the directory `plugins/ProjectReports`
+3. Clone this repository into the folder `plugins/ProjectReports`
+
+Note: Plugin folder is case-sensitive.
+
+Documentation
+-------------
+
+TODO.
